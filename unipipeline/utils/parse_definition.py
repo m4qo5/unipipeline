38ebf5e566d42892881a5fdaa320bcaed82ba507
alxe.master <alxe.master@gmail.com>
2021-06-25 00:09:56 +0300

    add create functions for workers/brokers/messages/waitings

--- unipipeline/utils/parse_definition.py
+++ unipipeline/utils/parse_definition.py
@@ -1,15 +1,16 @@
-from typing import Dict, Any, Set, Iterator
+from typing import Dict, Any, Set, Iterator, Tuple
 
 
-def parse_definition(definitions: Dict[str, Any], sys_names: Set[str] = None) -> Iterator[Dict[str, Any]]:
+def parse_definition(definitions: Dict[str, Any], defaults: Dict[str, Any], sys_names: Set[str] = None) -> Iterator[Tuple[str, Dict[str, Any]]]:
     if sys_names is None:
         sys_names = set()
 
     assert isinstance(definitions, dict)
     assert isinstance(sys_names, set)
 
-    default = definitions["__default__"]
-    assert isinstance(definitions["__default__"], dict), f"definitions['__default__'] must be dict. {type(definitions['__default__'])} given"
+    defaults = dict(defaults)
+    defaults.update(definitions.get("__default__", dict()))
+
     for name, raw_definition in definitions.items():
         name = str(name)
         if name.startswith("_"):
@@ -20,9 +21,9 @@ def parse_definition(definitions: Dict[str, Any], sys_names: Set[str] = None) ->
             else:
                 raise ValueError(f"key '{name}' is not acceptable")
         assert isinstance(raw_definition, dict)
-        definition = dict(default)
+        definition = dict(defaults)
         definition.update(raw_definition)
 
         definition["name"] = name
 
-        yield definition
+        yield name, definition
