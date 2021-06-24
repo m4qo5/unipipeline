08af12ab95abb44c0c57561e3f5d19c5b78a5cb1
alxe.master <alxe.master@gmail.com>
2021-06-25 00:12:05 +0300

    update example

---
+++ example/main.py
@@ -0,0 +1,22 @@
+import logging
+import os.path
+
+from unipipeline import Uni
+
+
+logging.basicConfig(
+    level=os.environ.get('LOGLEVEL', logging.DEBUG),
+    format='%(asctime)s | %(name)s | %(levelname)s | %(message)s'
+)
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+u = Uni(f"{CWD}/dag.yml")
+
+u.check_load_all(create=True)
+
+u.get_worker("input_worker").send(dict())
+
+u.get_worker("input_worker").consume()
+
+print("it works!")
