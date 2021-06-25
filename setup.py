8e540ae4bb067ecc361c0b108961e75a0703c7f3
Aliaksandr Master <alxe.master@gmail.com>
2021-06-25 18:55:19 +0300

    add requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="unipipeline",
-    version="1.0.7",
+    version="1.1.0",
     description="simple way to build the declarative and distributed data pipelines with python. it supports rabbitmq or kafka as a broker",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -30,5 +30,5 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["jinja2", "pyyaml", "kafka-python", "pika", "pydantic"]
+    install_requires=["jinja2", "pyyaml", "kafka-python", "pika", "pydantic", "crontab"]
 )
