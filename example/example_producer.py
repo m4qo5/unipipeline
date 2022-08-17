9d38560053fd36c552a90d4c55b8360badbc38ce
Minkevich Denis <d.minkevich@unic-lab.by>
2022-08-17 10:31:24 +0300

    add example auto retry worker with infinity loop of heardbeating. fix some file permissions to 755

--- example/example_producer.py
+++ example/example_producer.py
@@ -13,7 +13,7 @@ from unipipeline.modules.uni import Uni
 u = Uni(f"{CWD}/dag-{args.type}.yml")
 
 u.init_producer_worker('input_worker')
-
+u.init_producer_worker('auto_retry_worker')
 u.initialize()
 
 for i in range(args.produce_count):
@@ -23,6 +23,7 @@ for i in range(args.produce_count):
         batch.append(dict(value=i, some=some))
     u.send_to("input_worker", batch)
     # if i % 1000 == 0:
+    u.send_to("auto_retry_worker", batch)
     #     sleep(14)
     print('>> SENT at', datetime.now())  # noqa
     sleep(args.delay)
